9bb22290a53984d6217e405e86f3acac698ed826
HarrisonLoh <46823418+harrisonloh@users.noreply.github.com>
2022-04-12 18:58:22 -0500

    maps started to work, small error

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
-function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.835471, lng: -87.625888},
+    zoom: 8
+  });
 }
-
 google.maps.event.addDomListener(window, 'load', initMap);
