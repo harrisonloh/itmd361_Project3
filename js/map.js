0ec23e07f886bdc5d2ec29d5096dcd2dd9c94a73
HarrisonLoh <46823418+harrisonloh@users.noreply.github.com>
2022-04-12 19:30:46 -0500

    fixing map css

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.835471, lng: -87.625888},
-    zoom: 8
+    zoom: 12
   });
 }
 google.maps.event.addDomListener(window, 'load', initMap);
